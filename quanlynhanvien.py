06db159b222c1be8c3d3432d3fe1bbf182ba7e65
Doan Thuc Dinh <doanthucdinh2006nt@gmail.com>
2025-10-24 12:13:36 +0700

    PS46587_Doãn Thúc Định_ASM final

--- quanlynhanvien.py
+++ quanlynhanvien.py
@@ -1,3 +1,4 @@
+import csv
 from nhanvien import *
 from typing import List, Optional
 
@@ -40,6 +41,8 @@ class QuanLyNhanVien:
             self.dsnv.append(nv)
             print(f"Đã thêm: {nv}")
 
+            self.luu_file_csv()
+
 # Y2 Đọc và xuất danh sách nhân viên ra màn hình.
     def xuat_dsnv(self):
         if not self.dsnv:
@@ -48,6 +51,20 @@ class QuanLyNhanVien:
         print("\n=== DANH SÁCH NHÂN VIÊN ===")
         for nv in self.dsnv:
             print(nv)
+
+ # Lưu ra file CSV
+    def luu_file_csv(self, file="nhanvien.csv"):
+        with open(file, "w", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            for nv in self.dsnv:
+                if isinstance(nv, TiepThi):
+                    writer.writerow([nv.ma_nv, nv.ho_ten, "Tiếp thị", nv.luong, nv.doanh_so, nv.hoa_hong])
+                elif isinstance(nv, TruongPhong):
+                    writer.writerow([nv.ma_nv, nv.ho_ten, "Trưởng phòng", nv.luong, nv.luong_trach_nhiem])
+                else:
+                    writer.writerow([nv.ma_nv, nv.ho_ten, "Hành chính", nv.luong])
+        print("Đã lưu danh sách vào nhanvien.csv")
+
 
     #Y3 Tìm và hiển thị nhân viên theo mã
     def tim_theo_ma(self, ma: str) -> Optional[NhanVien]:
@@ -63,6 +80,7 @@ class QuanLyNhanVien:
         for nv in self.dsnv:
             if nv.ma_nv == ma:
                 self.dsnv.remove(nv)
+                self.luu_file_csv()
                 print("Đã xóa nhân viên:", nv.ho_ten)
                 return
         print("Không tìm thấy nhân viên cần xóa.")
@@ -77,6 +95,7 @@ class QuanLyNhanVien:
                 luong_moi= input(f"Lương({nv.luong}):").strip()
                 if luong_moi:
                     nv.luong = float(luong_moi)
+                    self.luu_file_csv()
                 print("Lương nhập không hợp lệ.")
             print("Cập nhật thành công")
     #Y6 Tìm các nhân viên theo khoảng lương
@@ -103,6 +122,24 @@ class QuanLyNhanVien:
         for nv in top5:
             print(nv)
 
+    # Đọc danh sách nhân viên từ file CSV
+    def doc_file_csv(self, file="nhanvien.csv"):
+        try:
+            with open(file, "r", encoding="utf-8") as f:
+                reader = csv.reader(f)
+                self.dsnv.clear()
+                for row in reader:
+                    loai = row[2]
+                    if loai == "Hành chính":
+                        nv = NhanVien(row[0], row[1], float(row[3]))
+                    elif loai == "Tiếp thị":
+                        nv = TiepThi(row[0], row[1], float(row[3]), float(row[4]), float(row[5]))
+                    elif loai == "Trưởng phòng":
+                        nv = TruongPhong(row[0], row[1], float(row[3]), float(row[4]))
+                    self.dsnv.append(nv)
+            print("Đọc file CSV thành công.")
+        except FileNotFoundError:
+            print("Chưa có file nhanvien.csv.")
 
         
 
